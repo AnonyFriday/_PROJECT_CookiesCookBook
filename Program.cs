b383940b73c2826b04365364761b773747890f0f
SE182407_VuKimDuy <duyvkse182407@fpt.edu.vn>
2024-05-29 20:16:44 +0700

    Setup base project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _03_CookiesCookbook_Practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CookieCookbook cookieCookbookApp = new CookieCookbook();
+            cookieCookbookApp.Run();
+        }
+    }
+}
