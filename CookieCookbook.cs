22c06323ac98e9b96db94081c2e9ae7f9b50ca13
SE182407_VuKimDuy <duyvkse182407@fpt.edu.vn>
2024-05-29 20:28:08 +0700

    Added high level design on the application

--- CookieCookbook.cs
+++ CookieCookbook.cs
@@ -8,7 +8,39 @@ namespace _03_CookiesCookbook_Practise
 {
     internal class CookieCookbook
     {
+        private readonly RecipeRepository _recipeRepository;
+        private readonly RecipeUserInteraction _recipeUserInteraction;
 
-        public void Run();
+        public CookieCookbook(RecipeRepository recipeRepository, RecipeUserInteraction recipeUserInteraction)
+        {
+            _recipeRepository = recipeRepository;
+            _recipeUserInteraction = recipeUserInteraction;
+        }
+
+        public void Run()
+        {
+            var allRecipes = _recipeRepository.Read(filePath);
+            _recipeUserInteraction.PrintExistingRecipes(allRecipes);
+            _recipeUserInteraction.PromptToCreateRecipe();
+
+            var incredients = _recipeUserInteraction.ReadIngredientsFromUser();
+
+            if (incredients.Count > 0)
+            {
+                var recipe = new Recipe(ingredients);
+                allRecipes.Add(recipe);
+                _recipeRepository.Write(filePath, allRecipes);
+                _recipeUserInteraction.ShowMessage("Recipe added: ");
+                _recipeUserInteraction.ShowMessage(recipe.ToString());
+            }
+
+            else
+            {
+                _recipeUserInteraction.ShowMessage(
+                    "No ingredients have been selected." +
+                    "Recipe will not be saved.");
+            }
+
+        }
     }
 }
---
+++ RecipeRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace _03_CookiesCookbook_Practise
+{
+    internal class RecipeRepository
+    {
+    }
+}
